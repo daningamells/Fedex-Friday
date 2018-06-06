a30072ba2ef54ec284169ce3ddc70bfb9c94c27a
Daniel Ingamells <dan_ingamells@hotmail.co.uk>
2018-06-06 01:15:11 +0100

    06/06/18
    06/06/18

--- js/script.js
+++ js/script.js
@@ -1,27 +1,23 @@
 $.ajax({
     url: "https://raw.githubusercontent.com/daningamells/random-quote-v2/master/GlobalFirePower.csv",
     async: false,
-    success: function (csvd) {
+    success: function(csvd) {
         data = $.csv.toObjects(csvd);
     },
     dataType: "text",
-    complete: function () {
+    complete: function() {
         // call a function on complete
     }
 });
 
-
-console.log(data[101].Country);
-
-
-var csvImport = data
+//var csvImport = data
 /*Header in default file:
 country ISO3 Rank Total Population Manpower Available Fit-for-Service	Reaching Military Age	Total Military Personnel	Active Personnel	Reserve Personnel	Total Aircraft Strength	Fighter Aircraft	Attack Aircraft	Transport Aircraft	Trainer Aircraft	Total Helicopter Strength	Attack Helicopters	Combat Tanks	Armored Fighting Vehicles	Self-Propelled Artillery	Towed Artillery	Rocket Projectors	Total Naval Assets	Aircraft Carriers	Frigates	Destroyers	Corvettes	Submarines	Patrol Craft	Mine Warfare Vessels	Production (bbl/dy)	Consumption (bbl/dy)	Proven Reserves (bbl)	Labor Force	Merchant Marine Strength	Major Ports / Terminals	Roadway Coverage (km)	Railway Coverage (km)	Serivecable Airports	Defense Budget	External Debt	Foreign Exchange / Gold	Purchasing Power Parity	Square Land Area (km)	Coastline (km)	Shared Borders (km)	Waterways (km)
 */
 //modifiers
 //manpower
-var actPersonel_Mod = 0.5;
-var resPersonel_Mod = 0.5;
+var actPersonel_Mod = 0.5; //Not in use
+var resPersonel_Mod = 0.5; //Not in use
 var man_Mod = 0.25;
 //air
 var totAir_Mod = 0.7;
@@ -36,9 +32,9 @@ var rock_Mod = 0.15;
 var land_Mod = 0.25;
 //sea
 var airCar_Mod = 0.15;
-var ships_Mod = 0.45;
-var subs_Mod = 0.2;
-var local_Mod = 0.1;
+var ships_Mod = 0.5;
+var subs_Mod = 0.25;
+var local_Mod = 0.1; //Not in use
 var sea_Mod = 0.25;
 //support
 var finance_Mod = 0.6;
@@ -48,56 +44,63 @@ var mil_Mod = 0.6;
 var sup_Mod = 0.4;
 
 var newData = [];
+var cdata = {};
 
+function index(name, column) {
+    var score = [];
+    for (i = 0; i < data.length; i++) {
+        var columnScore = parseInt(data[i][column]);
+        score.push(columnScore);
+    }
 
-// function index () {
-// var score = [];
-// for (i = 0; i < data.length; i++) {
-// var armyScore = parseInt(data[i]["Total Military Personnel"]);
-// score.push(armyScore);
-// }
-//
-// for (i = 0; i < data.length; i++) {
-// var highScore = Math.max(...score);
-// console.log(highScore);
-// armyScore = parseInt(data[i]["Total Military Personnel"]);
-// manpowerScore = armyScore / highScore * 100;
-// var country = data[i].Country;
-// newData.push({country, manpowerScore});
-// }
-//
-// };
-//
-// index();
+    for (i = 0; i < data.length; i++) {
 
+        var highScore = Math.max(...score);
+        columnScore = parseInt(data[i][column]);
+        var indexScore = columnScore / highScore * 100;
+        var country = data[i].Country;
 
-function index (name, column) {
-var score = [];
-for (i = 0; i < data.length; i++) {
-var columnScore = parseInt(data[i][column]);
-score.push(columnScore);
-}
+        if (typeof newData[i] == "undefined") {
+            newData.push({
+                Country: data[i]["Country"],
+                [name]: indexScore
+            });
+        } else {
 
-for (i = 0; i < data.length; i++) {
+            newData[i][name] = indexScore;
 
-var highScore = Math.max(...score);
-columnScore = parseInt(data[i][column]);
-var indexScore = columnScore / highScore * 100;
-var country = data[i].Country;
+        }
 
-if (typeof newData[i] == "undefined") {
-   newData.push({Country : data[i]["Country"], [name] : indexScore});
-}
-else {
-
-newData[i][name] = indexScore;
-
-}
-
-}
-var score = [];
+    }
+    var score = [];
 };
 
+//gives miltary score
+function rawScore() {
+    //Manpower = 1 & 2
+    //Airpower = 3 & 4
+    //Army = 5, 6, 7, 8 , 9
+    //Navel = 10, 11, 12, 13
+    //var scores[8][sel.length];
+    //loop through each row to get the score for each section
+    for (var row = 0; row < newData.length; row++) {
+        //Military Scores
+        newData[row]["Personnel Score"] = /*actPersonel_Mod * */ newData[row]['Total Military Personnel']; // + resPersonel_Mod * indexResults.[2].[row];
+        newData[row]["Air Score"] = totAir_Mod * newData[row]['Total Aircraft Strength'] + totHel_Mod * newData[row]['Total Helicopter Strength'];
+        newData[row]["Land Score"] = tanks_Mod * newData[row]['Combat Tanks'] + armor_Mod * newData[row]['Armored Fighting Vehicles'] + spArt_Mod * newData[row]['Self-Propelled Artillery'] + towArt_Mod * newData[row]['Towed Artillery'] + rock_Mod * newData[row]['Rocket Projectors'];
+        newData[row]["Naval Score"] = airCar_Mod * newData[row]['Aircraft Carriers'] + ships_Mod * (newData[row]['Frigates'] + newData[row]['Corvettes'] + newData[row]['Destroyers']) + subs_Mod * newData[row]['Submarines']; // + local_Mod * indexResults.[13].[row];
+        newData[row]["Military Score"] = man_Mod * newData[row]["Personnel Score"] + air_Mod * newData[row]["Air Score"] + land_Mod * newData[row]["Land Score"] + sea_Mod * newData[row]["Naval Score"] //Total Military Score
+        //Support Score
+        newData[row]["Support Score"] = /*finance_Mod * indexResults.[14].[row] + labour_Mod * */ newData[row]["Labor Force"];
+        //Combined Score
+        newData[row]["Total Score"] = mil_Mod * newData[row]["Military Score"] + sup_Mod * newData[row]["Support Score"]
+    }
+
+    //return scores;
+
+}
+
+//Indexing
 index('Total Military Personnel', 'Total Military Personnel');
 index('Total Aircraft Strength', 'Total Aircraft Strength');
 index('Total Helicopter Strength', 'Total Helicopter Strength');
@@ -112,111 +115,124 @@ index('Destroyers', 'Destroyers');
 index('Corvettes', 'Corvettes');
 index('Submarines', 'Submarines');
 index('Labor Force', 'Labor Force');
+rawScore();
 
+function testFight() {
+    fight([{
+        Country: 'Russia',
+        Team: 'A'
+    }, {
+        Country: 'China',
+        Team: 'D'
+    }, {
+        Country: 'Japan',
+        Team: 'D'
+    }]);
+}
 
+function fight(teams) {
+    var teamA = {
+        'Personnel Score': 0,
+        'Air Score': 0,
+        'Land Score': 0,
+        'Naval Score': 0,
+        'Military Score': 0,
+        'Support Score': 0,
+        'Total Score': 0
+    };
+    var teamD = {
+        'Personnel Score': 0,
+        'Air Score': 0,
+        'Land Score': 0,
+        'Naval Score': 0,
+        'Military Score': 0,
+        'Support Score': 0,
+        'Total Score': 0
+    };
+    for (var i = 0; i < teams.length; i++) {
+        for (var j = 0; j < newData.length; j++) {
+            if (teams[i].Country == newData[j].Country) {
+                if (teams[i].Team == 'A') {
+                    console.log(teams[i].Country);
+                    teamA['Personnel Score'] += newData[j]['Personnel Score'];
+                    teamA['Air Score'] += newData[j]['Air Score'];
+                    teamA['Land Score'] += newData[j]['Land Score'];
+                    teamA['Naval Score'] += newData[j]['Naval Score'];
+                    teamA['Military Score'] += newData[j]['Military Score'];
+                    teamA['Support Score'] += newData[j]['Support Score'];
+                    teamA['Total Score'] += newData[j]['Total Score'];
+                    console.log('Team A Score: ' + teamA['Total Score']);
 
+                } else {
+                    console.log(teams[i].Country);
+                    teamD['Personnel Score'] += newData[j]['Personnel Score'];
+                    teamD['Air Score'] += newData[j]['Air Score'];
+                    teamD['Land Score'] += newData[j]['Land Score'];
+                    teamD['Naval Score'] += newData[j]['Naval Score'];
+                    teamD['Military Score'] += newData[j]['Military Score'];
+                    teamD['Support Score'] += newData[j]['Support Score'];
+                    teamD['Total Score'] += newData[j]['Total Score'];
+                    console.log('Team D Score: ' + teamD['Total Score']);
+                }
+            }
+        }
+    }
+    if (teamA['Total Score'] > teamD['Total Score']) {
+        alert('Attackers are Victorious!');
+    } else if (teamA['Total Score'] < teamD['Total Score']) {
+        alert('Defenders are Victorious!');
+    } else {
+        alert('It is a draw!');
+    }
+}
 
+$(function() {
+    for (i = 0; i < data.length; i++) {
+        var countryCode = data[i].ISO3;
+        var newCountryCode = getCountryISO2(countryCode);
+        var totalScore = newData[i]['Total Score'].toFixed(0).toString();
+        cdata[newCountryCode] = totalScore;
 
+    }
+    $('#world-map').vectorMap({
 
+        map: 'world_mill',
+        backgroundColor: '#36b55c',
+        regionsSelectable: true,
+        color: '#dfdfdd',
+        hoverOpacity: 0,
+        selectedColor: '#5f8b98',
 
+        series: {
+            //this is the object for passing country/region data into
+            regions: [{
+                //define the range of color values
+                scale: ['#DEEBF7', '#08519C'],
+                //define the function that maps data to color range
+                normalizeFunction: 'linear',
+                //define the coloration method
+                attribute: 'fill',
+                //define the array of country data
+                values: cdata,
+                legend: {
+                    vertical: true,
+                    title: 'Military Score'
 
+                }
+            }]
+        },
 
+        onRegionClick: function(event, code) {
+            var map = $('#world-map').vectorMap('get', 'mapObject');
+            var regionName = map.getRegionName(code);
+            console.log(regionName);
 
+        },
 
+        onRegionTipShow: function(e, el, code) {
+            el.html(el.html() + ' (Score - ' + cdata[code] + ')');
+        }
 
+    });
 
-
-
-// //position 0 is country name
-//
-// function importCSV()
-// {
-// 	//makes dummy data
-// }
-//
-// //takes an array of countries selected and ranks each column accordingly
-// function indexSelection(sel)
-// {
-// 	var selData = pullData(sel);
-// 	var indexResults[16][sel.length];
-// 	//loops through and index each column
-// 	for(var col = 0; col < selData[0].length; col++)
-// 	{
-// 		var max_value = 0;
-// 		//gets max value
-// 		for (var row = 0; row < selData.length; row++)
-// 		{
-// 			//sets max value on 0th case
-// 			if(row == 0)
-// 			{
-// 				max_value = selData[col][row];
-// 			}
-// 			else
-// 			{
-// 				//checks if new vale is larger
-// 				if(selData[col][row] > max_value)
-// 				{
-//           //fixed my code so you don't need to change this bit as discussed.
-// 					max_value = selData[col][row];
-// 				}
-// 			}
-// 		}
-//     //Gets index out of 100
-//   	for (var row = 0; row < selData.length; row++)
-//   	{
-//   		indexResults[col][row] = (selData[col][row] / max_value) * 100
-//   	}
-// 	}
-// 	return indexResults;
-// }
-//
-// //combines list data with new data
-// function pullData(sel)
-// {
-// 	var results[16][sel.length]
-// 	var length = sel.length;
-// 	for(var i = 0; i < length; i++)
-// 	{
-// 		//checks for a country match. I need to make an error for the case that it can't find it.
-// 		var counter = 0;
-// 		while(sel[i] != importCSV[counter][0] && counter < 1000)
-// 		{
-// 			counter++;
-// 		}
-// 		//might need to loop though all the loops
-// 		results[i] = importCSV[counter];
-// 	}
-// 	return results;
-// }
-//
-// //gives miltary score
-// function rawScore(sel)
-// {
-// 	var indexResults[16][sel.length] = indexSelection(sel);
-// 	//Manpower = 1 & 2
-// 	//Airpower = 3 & 4
-// 	//Army = 5, 6, 7, 8 , 9
-// 	//Navel = 10, 11, 12, 13
-// 	var scores[8][sel.length];
-// 	//loop through each row to get the score for each section
-// 	for(var row = 0; row < indexResults.[0].length; row++)
-// 	{
-// 		//Country Name
-// 		score[0].[row] = indexResults.[0].[row];
-// 		//Military Scores
-// 		score[1].[row] = actPersonel_Mod * indexResults.[1].[row] + resPersonel_Mod * indexResults.[2].[row];
-// 		score[2].[row] = totAir_Mod * indexResults.[3].[row] + totHel_Mod * indexResults.[4].[row];
-// 		score[3].[row] = tanks_Mod * indexResults.[5].[row] + armor_Mod * indexResults.[6].[row] + spArt_Mod * indexResults.[7].[row] + towArt_Mod * indexResults.[8].[row] + rock_Mod * indexResults.[9].[row];
-// 		score[4].[row] = airCar_Mod * indexResults.[10].[row] + ships_Mod * indexResults.[11].[row] + subs_Mod * indexResults.[12].[row] + local_Mod * indexResults.[13].[row];
-// 		score[5].[row] = man_Mod * score[1].row + air_Mod * score[2].row + land_Mod * score[3].row + sea_Mod *  score[4].row  //Total Military Score
-// 		//Support Score
-// 		score[6].[row] = finance_Mod * indexResults.[14].[row] + labour_Mod * indexResults.[15].[row];
-// 		//Combined Score
-// 		score[7].[row] = mil_Mod * score[5].row + sup_Mod * score[6].row
-// 	}
-//
-// 	return scores;
-//
-// }
-//
-// Make a github and tell me how to get there on whatapp (email won't work while I am away)
+});
